96d3ecd38614d1e2e2d452e3a9f4afb99cb7b738
pergran1 <pergranberg@hotmail.com>
2022-11-16 07:51:52 +0100

    Added EC2 script

--- S3/Creating_s3_buckets_insert_data.py
+++ S3/Creating_s3_buckets_insert_data.py
@@ -1,1 +1,11 @@
 # This is code that will create S3 bucket and insert a csv
+# This code will be used on a lambda function, but it can run in a python local
+
+import boto3
+
+s3 = boto3.client('s3')
+s3.create_bucket(Bucket="created_by_lambda",
+                 CreateBucketConfiguration={
+                     'LocationConstraint': 'eu-north-1',
+                 },
+                 )
