daf591fed09bd45583578500d8b410621c483489
pergran1 <pergranberg@hotmail.com>
2022-11-28 22:29:06 +0100

    Added create ec2 instance

---
+++ EC2/Create_EC2.py
@@ -0,0 +1,18 @@
+import os
+import boto3
+
+ec2 = boto3.resource('ec2')
+
+def create_ec2():
+    instance = ec2.create_instances(
+        ImageId='ami-02aeff1a953c5c2ff',
+        InstanceType='t3.micro',
+        KeyName='lambdaEC2keyPair',
+        SubnetId='subnet-0ca596c43c463291e',
+        MaxCount=1,
+        MinCount=1
+    )
+
+    print("New insteance created:", instance[0].id)
+
+create_ec2()
